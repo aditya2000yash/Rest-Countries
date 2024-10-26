d552046b8bc89b0df966dd4c58e7c1ac96bcbcaa
Aditya Ranjan <ranjan.aditya0711@gmail.com>
2024-10-26 15:32:01 +0530

    Implemented React Router setup with Navbar, HomePage, DetailsPage, and ErrorPage components

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+// import HomePage from "./Pages/Homepage";
+import DetailsPage from "./Pages/DetailsPage";
+import ErrorPage from "./components/ErrorPage";
+import HomePage from "./Pages/Home";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/country/:code" element={<DetailsPage />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
