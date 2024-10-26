a524ea6053ab3d5b7fdff9640d0d47d7d42467b9
Aditya Ranjan <ranjan.aditya0711@gmail.com>
2024-10-26 15:36:59 +0530

    Added CardsList component to render a list of country cards or display 'No results found' message.

---
+++ src/components/CardsList.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import CardsContainer from "./CardsContainer";
+
+const CardsList = ({ filteredResponse }) => {
+  return (
+    <div className="flex mt-10 flex-wrap gap-10 justify-center items-center px-[1rem]">
+      {filteredResponse.length === 0 ? (
+        <p>No results found</p> 
+      ) : (
+        filteredResponse.map((item, idx) => (
+          <CardsContainer key={idx} item={item} />
+        ))
+      )}
+    </div>
+  );
+};
+
+export default CardsList;
