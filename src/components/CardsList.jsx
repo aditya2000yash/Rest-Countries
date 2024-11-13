0b613101ea09f51b0e47d68b0b150098cef49dec
Aditya Ranjan <ranjan.aditya0711@gmail.com>
2024-11-13 09:15:21 +0530

    Removed unnecessary comments.

--- src/components/CardsList.jsx
+++ src/components/CardsList.jsx
@@ -1,31 +1,10 @@
-// import React from "react";
-// import CardsContainer from "./CardsContainer";
-
-// const CardsList = ({ filteredResponse }) => {
-//   return (
-//     <div className="flex mt-10 flex-wrap gap-10 justify-center items-center px-[1rem]">
-//       {filteredResponse.length === 0 ? (
-//         <p>No results found</p> // Handle no results case
-//       ) : (
-//         filteredResponse.map((item, idx) => (
-//           <CardsContainer key={idx} item={item} />
-//         ))
-//       )}
-//     </div>
-    
-//   );
-  
-// };
-
-// export default CardsList;
-
 import React from "react";
 import CardsContainer from "./CardsContainer";
 
 const CardsList = ({ filteredResponse }) => {
   return (
     <div className="flex mt-10 flex-wrap gap-10 justify-center items-center px-[1rem]">
-      {filteredResponse.length === 0 ? (
+      {(filteredResponse || []).length === 0 ? (
         <p>No results found</p>
       ) : (
         filteredResponse.map((item, idx) => <CardsContainer key={idx} item={item} />)
